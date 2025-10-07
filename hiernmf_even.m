eb9b91733807be97274dee9648a22db91652ccc4
ben <benjamincobb00@gmail.com>
2025-10-07 17:23:23 -0400

    replace compute_priority with just setting to one

--- hiernmf_even.m
+++ hiernmf_even.m
@@ -261,7 +261,7 @@ function [subset, W_buffer_one, H_buffer_one, priority_one] = trial_split(trial_
 		priority_one = -2;
 	end
 
-	priority_one = 1;
+	% priority_one = 1;
 end
 
 %--------------------------------------
@@ -285,60 +285,60 @@ function [cluster_subset, W_buffer_one, H_buffer_one, priority_one] = actual_spl
 		W_buffer_one(term_subset, :) = W;
 		H_buffer_one = H;
 		if length(unique(cluster_subset)) > 1
-			priority_one = compute_priority(W_parent, W_buffer_one);
+			priority_one = 1; %compute_priority(W_parent, W_buffer_one);
 		else
 			priority_one = -1;
 		end
 	end
 end
 
-%--------------------------------------
+% %--------------------------------------
 
-function priority = compute_priority(W_parent, W_child)
+% function priority = compute_priority(W_parent, W_child)
 
-	n = length(W_parent);
-	[sorted_parent, idx_parent] = sort(W_parent, 'descend');
-	[sorted_child1, idx_child1] = sort(W_child(:, 1), 'descend');
-	[sorted_child2, idx_child2] = sort(W_child(:, 2), 'descend');
+% 	n = length(W_parent);
+% 	[sorted_parent, idx_parent] = sort(W_parent, 'descend');
+% 	[sorted_child1, idx_child1] = sort(W_child(:, 1), 'descend');
+% 	[sorted_child2, idx_child2] = sort(W_child(:, 2), 'descend');
 
-	n_part = length(find(W_parent ~= 0));
-	if n_part <= 1
-		priority = -3;
-	else
-		weight = log(n:-1:1)';
-		first_zero = find(sorted_parent == 0, 1); 
-		if length(first_zero) > 0 
-			weight(first_zero:end) = 1;
-		end
-		weight_part = zeros(n, 1);
-		weight_part(1:n_part) = log(n_part:-1:1)';
-		[sorted, idx1] = sort(idx_child1);
-		[sorted, idx2] = sort(idx_child2);
-		max_pos = max(idx1, idx2);
-		discount = log(n-max_pos(idx_parent)+1);
-		discount(discount == 0) = log(2);
-		weight = weight ./ discount;
-		weight_part = weight_part ./ discount;
-		priority = NDCG_part(idx_parent, idx_child1, weight, weight_part) * NDCG_part(idx_parent, idx_child2, weight, weight_part);
-	end
-end
+% 	n_part = length(find(W_parent ~= 0));
+% 	if n_part <= 1
+% 		priority = -3;
+% 	else
+% 		weight = log(n:-1:1)';
+% 		first_zero = find(sorted_parent == 0, 1); 
+% 		if length(first_zero) > 0 
+% 			weight(first_zero:end) = 1;
+% 		end
+% 		weight_part = zeros(n, 1);
+% 		weight_part(1:n_part) = log(n_part:-1:1)';
+% 		[sorted, idx1] = sort(idx_child1);
+% 		[sorted, idx2] = sort(idx_child2);
+% 		max_pos = max(idx1, idx2);
+% 		discount = log(n-max_pos(idx_parent)+1);
+% 		discount(discount == 0) = log(2);
+% 		weight = weight ./ discount;
+% 		weight_part = weight_part ./ discount;
+% 		priority = NDCG_part(idx_parent, idx_child1, weight, weight_part) * NDCG_part(idx_parent, idx_child2, weight, weight_part);
+% 	end
+% end
 
-%--------------------------------------
+% %--------------------------------------
 
-function score = NDCG_part(ground, test, weight, weight_part)
+% function score = NDCG_part(ground, test, weight, weight_part)
 
-	[sorted, seq_idx] = sort(ground);
-	weight_part = weight_part(seq_idx);
+% 	[sorted, seq_idx] = sort(ground);
+% 	weight_part = weight_part(seq_idx);
 
-	n = length(test);
-	uncum_score = weight_part(test);
-	uncum_score(2:n) = uncum_score(2:n) ./ log2(2:n)';
-	cum_score = cumsum(uncum_score);
+% 	n = length(test);
+% 	uncum_score = weight_part(test);
+% 	uncum_score(2:n) = uncum_score(2:n) ./ log2(2:n)';
+% 	cum_score = cumsum(uncum_score);
 
-	ideal_score = sort(weight, 'descend');
-	ideal_score(2:n) = ideal_score(2:n) ./ log2(2:n)';
-	cum_ideal_score = cumsum(ideal_score);
+% 	ideal_score = sort(weight, 'descend');
+% 	ideal_score(2:n) = ideal_score(2:n) ./ log2(2:n)';
+% 	cum_ideal_score = cumsum(ideal_score);
 
-	score = cum_score ./ cum_ideal_score;
-	score = score(end);
-end
+% 	score = cum_score ./ cum_ideal_score;
+% 	score = score(end);
+% end
